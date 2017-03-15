25dd9b0b0822b73241877f268325d40c4b046596
s3502942 <s3502942@student.rmit.edu.au>
2017-03-15 14:04:48 +1100

    added an incomplete master.blade file and edited welcome.blade file

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<meta name="description" content="" />
+	<meta name="author" content="" />
+	<title>@yield('title')</title>
+	<!-- BOOTSTRAP CORE STYLE CSS -->
+	<link href="">
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	<!-- FONT AWESOME CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+	<!-- FLEXSLIDER CSS -->
+	<link rel="stylesheet" href="{{ asset('assets/css/flexslider.css') }}">
+	<!-- CUSTOM STYLE CSS -->
+	<link rel="stylesheet" href="{{ asset('assets/css/style') }}">   
+	<!-- Google	Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css' />
+</head>
