c4c114d7b8a181cbaf3dc4d43fb87968ae388b9e
s3502942 <s3502942@student.rmit.edu.au>
2017-03-21 14:29:27 +1100

    migrated some pages for curriculum

---
+++ resources/views/curriculum/literacy.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.master')
+@section('title', 'Curriculum')
+@section('content')
+    <div class="row text-center">
+        <div id="features-sec" class="container set-pad" >
+            <div class="row text-center">
+                <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+                    <h1 data-scroll-reveal="enter from the bottom after 0.2s"  class="header-line">Literacy at Courtenay Gardens Primary School</h1>
+                    <p>In the Victorian Curriculum, the English domain is central to the learning and development of all students. It helps create confident communicators, imaginative thinkers and informed citizens. It is through the study of English that students learn to analyse, understand, communicate with and build relationships with others and with the world around them. In English, students are assessed under three areas: Reading and Viewing, Writing and Speaking and Listening.</p>
+
+                    <p>The English domain aims to ensure that students: </p>
+                    <ul>
+                        <li>Learn to listen to, read, view, speak, write, create and reflect on increasingly complex and sophisticated spoken and written texts across a growing range of contexts with accuracy, fluency and purpose</li>
+                        <li>Appreciate, enjoy and use the English language in all its variations and develop a sense of its richness and power to evoke feelings, convey information, form ideas, facilitate interaction with others, entertain, persuade and argue </li>
+                        <li>Understand how Standard Australian English works in its spoken and written forms and in combination with non-linguistic forms of communication to create meaning</li>
+                    </ul>
+                </div>
+
+                
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- storage/framework/views/0aa1d6ea0a2ccf83a254ca3f6d08999ce43349fa.php
+++ storage/framework/views/0aa1d6ea0a2ccf83a254ca3f6d08999ce43349fa.php
@@ -1,41 +1,34 @@
-<?php $__env->startSection('title', 'Welcome'); ?>
+<?php $__env->startSection('title', 'Curriculum'); ?>
 <?php $__env->startSection('content'); ?>
     <div class="row text-center">
         <div id="features-sec" class="container set-pad" >
             <div class="row text-center">
                 <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
-                    <h1 data-scroll-reveal="enter from the bottom after 0.2s"  class="header-line">ABOUT US</h1>
-                    <p data-scroll-reveal="enter from the bottom after 0.3s" >
-                        Courtenay Gardens Primary School is committed to ensuring that all children have fostered in them qualities and skills which will enable them to adapt to change and be lifelong learners.
-                    </p>
+                    <h1 data-scroll-reveal="enter from the bottom after 0.2s"  class="header-line">CURRICULUM</h1>
+                    <p>The challenging and engaging curriculum is designed to meet the needs of individual children and caters for all levels. Click on each subject to find out more information.</p>
+                    <a href="">Literacy</a><br>
+                    <a href="">Numeracy</a><br>
+                    <a href="">Digital Technologies</a><br>
+                    <a href="">Lote (Indonesian)</a><br>
+                    <a href="">The Arts - Visual and performing</a><br>
+                    <a href="">Integrated Units - Science, The Humanities, Communication, Civics and Citizenship, Thinking processes and Design, Creativty and Technology</a><br>
+                    <a href="">Health and Physical Education</a><br>
+                    <a href="">Drug Education</a><br>
+                    <a href="">Program for Students with Disabilities</a><br>
+                    <a href="">eSmart</a>
+                    <hr />
+                    <p>The school places particular emphasis on: </p>
+                    <ul>
+                            <li>The writing processes -Narrative, Procedure, Report, Recount, Persuasive, Personal and Descriptive</li>
+                            <li>Spelling</li>
+                            <li>Reading Comprehension</li>
+                            <li>Numeracy</li>
+                            <li>ICT - with a dedicated multimedia studio, a roving laptop program and two purpose-built computer labs</li>
+                    </ul>
                 </div>
 
-                <div class="col-4" data-scroll-reveal="enter from the bottom after 0.4s">
-                    <div class="about-div">
-                        <i class="fa fa-paper-plane-o fa-4x icon-round-border" ></i>
-                        <h3>CURRICULUMS PAGE </h3>
-                        <p>
-                            Grade sizes kept to a minimum<br>
-                            Straight grades (where possible)<br>
-                            Modern, well equipped school<br>
-                            Positive attitude to the wearing of Our compulsory school uniform<br>
-                            A clear code of coduct<br>
-                            High academic standards<br>
-                            Safe, secure environment<br>
-                        </p>
-                    </div>
-                </div>
+                
 
-                <div class="col-4" data-scroll-reveal="enter from the bottom after 0.6s">
-                    <div class="about-div">
-                        <i class="fa fa-magic fa-4x icon-round-border" ></i>
-                        <h3 >ONE TO ONE STUDY</h3>
-                        <p >
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                            Aenean commodo .
-
-                        </p>
-                        <a href="#" class="btn btn-info btn-set"  >ASK THE EXPERT</a>
                     </div>
                 </div>
             </div>
