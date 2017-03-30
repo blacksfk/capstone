659892aa6d7231fc474c64ef8e3e2890974d63d4
s3502942 <s3502942@student.rmit.edu.au>
2017-03-30 16:22:11 +1100

    Added stock image within galleries on home page, incomplete

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,14 +9,51 @@
                     <p>
                     Courtenay Gardens Primary School is committed to ensuring that all children have fostered in them qualities and skills which will enable them to adapt to change and be lifelong learners.
                     </p>
-                </div>
-
-                <div id="features-sec" class="container set-pad" >
-                    <div class="row text-center">
-                        <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
-                            <div class="row">
-                            </div>
+                    <h1 class="header-line">GALLERY</h1>
+                    <div class="row">
+                        <div class="col-lg-3 col-md-4 col-xs-6">
+                            <a class="thumbnail" href="#">
+                                <img class="img-responsive" src="assets/img/stock.jpeg" alt="">
+                            </a>
                         </div>
-                    </div>
+                        <div class="col-lg-3 col-md-4 col-xs-6">
+                            <a class="thumbnail" href="#">
+                                <img class="img-responsive" src="assets/img/stock.jpeg" alt="">
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-xs-6">
+                            <a class="thumbnail" href="#">
+                                <img class="img-responsive" src="assets/img/stock.jpeg" alt="">
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-xs-6">
+                            <a class="thumbnail" href="#">
+                                <img class="img-responsive" src="assets/img/stock.jpeg" alt="">
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-xs-6">
+                            <a class="thumbnail" href="#">
+                                <img class="img-responsive" src="assets/img/stock.jpeg" alt="">
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-xs-6">
+                            <a class="thumbnail" href="#">
+                                <img class="img-responsive" src="assets/img/stock.jpeg" alt="">
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-xs-6">
+                            <a class="thumbnail" href="#">
+                                <img class="img-responsive" src="assets/img/stock.jpeg" alt="">
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-xs-6">
+                            <a class="thumbnail" href="#">
+                                <img class="img-responsive" src="assets/img/stock.jpeg" alt="">
+                            </a>
+                        </div>
+                    </div> 
                 </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -10,8 +10,8 @@
         </div>
         <div class="navbar-collapse collapse move-me">
             <ul class="nav navbar-nav navbar-right">
-                    <li><a href="{{ url('') }}">HOME</a></li>
-                    <li class="dropdown dropdown-toggle">
+                    <li class="sliding-middle-out"><a href="{{ url('') }}">HOME</a></li>
+                    <li class="dropdown dropdown-toggle sliding-middle-out">
                         <a class="dropdown-toggle" data-toggle="dropdown" href="#">CURRICULUM
                             <span class="caret"></span></a>
                             <ul class="dropdown-menu">
@@ -19,8 +19,8 @@
                                 <li><a href="{{ url('/curriculum/numeracy') }}">Numeracy</a></li>
                                 <li><a href="{{ url('/curriculum/digital_technologies') }}">Digital Technologies</a></li>
                             </ul></li>
-                            <li><a href="{{ url('events') }}">EVENTS</a></li>
-                            <li class="dropdown dropdown-toggle">
+                            <li class="sliding-middle-out"><a href="{{ url('events') }}">EVENTS</a></li>
+                            <li class="dropdown dropdown-toggle sliding-middle-out">
                                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">GET INVOLVED
                                     <span class="caret"></span></a>
                                 <ul class="dropdown-menu">
@@ -28,8 +28,9 @@
                                     <li><a href="{{ url('parents') }}">For Parents</a></li>
                                     <li><a href="{{ url('teachers') }}">For Teachers</a></li>
                                 </ul></li>
-                            <li><a href="{{ url('contact') }}">CONTACT US</a></li>
-                            <li><a href="{{ route('admin.events.index') }}">ADMIN</a></li>
+                            <li class="sliding-middle-out"><a href="{{ url('contact') }}">CONTACT US</a></li>
+                            <li class="sliding-middle-out"><a href="{{ route('admin.events.index') }}">ADMIN</a></li>
+
             </ul>
         </div>
     </div>
