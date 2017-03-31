7a9c08cb977cfeed92be0bf4d3d81afb1c86ff8d
Reyham <s3496982@student.rmit.edu.au>
2017-03-31 15:25:41 +1100

    Modified dashboard
    - added upload gallery

---
+++ resources/views/shared/sidebar.blade.php
@@ -0,0 +1,7 @@
+<div class="well col-lg-2">
+			<p><b>Administration</b></p>
+			<a href="">Upload newsletter</a><br>
+			<a href="">Manage custom page</a><br>
+			<a href="">Edit calendar</a><br>
+            <a href="">Upload gallery images</a><br>
+		</div>
