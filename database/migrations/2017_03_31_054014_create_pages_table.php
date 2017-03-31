8fbb9f0fb0f6c64659baaae6d5a2b5eb81196a4f
Anthony <s3333113@student.rmit.edu.au>
2017-03-31 17:12:14 +1100

    created models and migrations for categories/pages

---
+++ database/migrations/2017_03_31_054014_create_pages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("name");
+            $table->tinyInteger("status");
+            $table->integer("category_id")->unsigned()->nullable();
+        });
+
+        Schema::table("pages", function(Blueprint $table) {
+            $table->foreign("category_id")->references("id")->on("categories");
+        })
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pages');
+    }
+}
