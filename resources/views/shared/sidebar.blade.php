9e6af3e6d8e61e54edb1abad493a2a4cc3471a5b
Anthony <s3333113@student.rmit.edu.au>
2017-04-02 19:18:06 +1000

    updated sidebar with routes

--- resources/views/shared/sidebar.blade.php
+++ resources/views/shared/sidebar.blade.php
@@ -1,9 +1,10 @@
 <div class="well col-lg-2">
-			<p><b>Administration</b></p>
-			<a href="{{ url('dashboard') }}">Landing Page</a><br>
-			<a href="">Upload newsletter</a><br>
-			<a href="">Manage custom page</a><br>
-			<a href="{{route('admin.events.index')}}">Add events</a><br>
-			<a href="">Edit calendar</a><br>
-            <a href="">Upload gallery images</a><br>
-		</div>
+    <p><b>Administration</b></p>
+    <a href="{{ url('admin') }}">Landing Page</a><br>
+    <a href="">Upload newsletter</a><br>
+    <a href="{{ route('admin.pages.index') }}">Page management</a><br>
+    <a href="{{ route('admin.events.index') }}">Event management</a><br>
+    <a href="{{ route('admin.links.index') }}">Link management</a><br>
+    <a href="">Edit calendar</a><br>
+    <a href="">Upload gallery images</a><br>
+</div>
