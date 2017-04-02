2f9df1fc3c3d28a743323775e11c14f77852fd4f
Anthony <s3333113@student.rmit.edu.au>
2017-04-02 22:33:27 +1000

    cleaned up sidebar - now uses li instead of br

--- resources/views/shared/sidebar.blade.php
+++ resources/views/shared/sidebar.blade.php
@@ -1,10 +1,12 @@
 <div class="well col-lg-2">
     <p><b>Administration</b></p>
-    <a href="{{ url('admin') }}">Landing Page</a><br>
-    <a href="">Upload newsletter</a><br>
-    <a href="{{ route('admin.pages.index') }}">Page management</a><br>
-    <a href="{{ route('admin.events.index') }}">Event management</a><br>
-    <a href="{{ route('admin.links.index') }}">Link management</a><br>
-    <a href="">Edit calendar</a><br>
-    <a href="">Upload gallery images</a><br>
+    <ul class="nav navbar-nav">
+        <li><a href="{{ url('admin') }}">Landing Page</a></li>
+        <li><a href="">Upload newsletter</a></li>
+        <li><a href="{{ route('admin.pages.index') }}">Page management</a></li>
+        <li><a href="{{ route('admin.events.index') }}">Event management</a></li>
+        <li><a href="{{ route('admin.links.index') }}">Link management</a></li>
+        <li><a href="">Edit calendar</a></li>
+        <li><a href="">Upload gallery images</a></li>
+    </ul>
 </div>
