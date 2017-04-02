914969ad2df0ef19a6a23d69958804ed05bdb788
Anthony <s3333113@student.rmit.edu.au>
2017-04-02 20:53:21 +1000

    created link controller

---
+++ app/Http/Controllers/LinkController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Link;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+
+class LinkController extends Controller
+{
+    private static $validation = [
+        "name" => "required",
+        "status" => "required"
+    ];
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view("admin.links.index")->with("links", Link::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("admin.links.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, self::$validation);
+        Link::create($request->all());
+
+        return redirect()->route("admin.links.index")->with("success", "Link successfully created");
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $link = Link::find($id);
+
+        return view("admin.links.edit")->with("link", $link);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, self::$validation);
+        $this->find($id)->update($request->all());
+
+        return redirect()->route("admin.links.index")->with("success", "Link successfully updated");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Link::find($id)->delete();
+
+        return redirect()->route("admin.links.index")->with("success", "Link successfully deleted");
+    }
+}
