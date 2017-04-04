d9b8e02a0ab89ba1e9c054bc67663b9003343800
s3502942 <s3502942@student.rmit.edu.au>
2017-04-04 21:19:53 +1000

    testing 5.0 contact page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="viewport" content="width=device-width">
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>@yield('title')</title>
