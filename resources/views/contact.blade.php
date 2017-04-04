b6a543b8ca83cd8b6204d41c4432075d3716c6c1
s3502942 <s3502942@student.rmit.edu.au>
2017-04-04 21:16:40 +1000

    testing 3.0 contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('title', 'Contact')
 @section('content')
-<meta name="viewport" content="width=device-width, initial-scale=You Scale (Ej: 0.5), user-scalable=yes">
+<meta name="viewport" content="width=device-width>
 
 <div id="features-sec" class="container set-pad" >
     <div class="row text-center">
