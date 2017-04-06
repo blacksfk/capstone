00ce08da3851fc546e23962443a4c7230dca8d90
s3502942 <s3502942@student.rmit.edu.au>
2017-04-07 00:13:53 +1000

    Fixed weird horizontal scroll bar and side whitespace. Hovering menu still in progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('title', 'Welcome')
 @section('content')
-    <div class="row text-center">
+
         <div id="features-sec" class="container set-pad" >
             <div class="row text-center">
                 <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
@@ -42,5 +42,5 @@
                 </div>
             </div>
         </div>
-    </div>
+
 @endsection
