d087ba35e3d3459e1bc9eb88c44029b9960d7644
s3502942 <s3502942@student.rmit.edu.au>
2017-04-07 00:26:28 +1000

    Forgotten to fix get involved pages

--- resources/views/involve/parents.blade.php
+++ resources/views/involve/parents.blade.php
@@ -1,133 +1,133 @@
 @extends('layouts.master')
 @section('title', 'For Parents')
 @section('content')
-    <div class="row text-center">
-        <div id="features-sec" class="container set-pad" >
-            <div class="row text-left">
-                <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
-                    <h1 class="header-line text-center">For Parents</h1>
-                    <p>After following the appropriate processes the School Council is extremely pleased to announce the replacement of the existing five Play Systems with new ones from a leading playground company. If you would like to see the drawings for each year level please click the following links:</p>
-                    <ul class ="text-center">
-                        <li><span class="glyphicon glyphicon-ok"></span> PREPS  -<a href=""> 4611517 </a></li>
-                        <li><span class="glyphicon glyphicon-ok"></span> YEAR 1 -<a href=""> 4611519 </a></li>
-                        <li><span class="glyphicon glyphicon-ok"></span> YEAR 2 -<a href=""> 4611520 </a></li>
-                        <li><span class="glyphicon glyphicon-ok"></span> YRS3/4 -<a href=""> 4611522 </a></li>
-                        <li><span class="glyphicon glyphicon-ok"></span> YRS5/6 -<a href=""> 4611542 </a></li>
-                    </ul>
-                    <hr>
-                    <h1 class="header-line text-center">CGPS Policies and Plans</h1>
-                    <p>Many policies/guidelines are dictated from the Department of Education & Training. Their website is <a href="">http://www.education.vic.gov.au/</a></p>
-                    <table border="1">
-                        <tr>
-                            <td><span class="style4 style11 style7 style10"><span class="style11 style13"><a href="2014AIP.pdf">CGPS Annual Implementation Plan </a></span></span></td>
-                        </tr>
-                        <tr>
-                            <td><span class="style4 style11 style7 style10"><span class="style11 style13"><a href="Annualreport2015.pdf">CGPS Annual Report to the Community </a></span></span></td>
-                        </tr>
-                        <tr>
-                            <td><span class="style4 style11 style7 style10"><span class="style11 style13"><a href="2014StratPlan.pdf">CGPS Strategic Plan</a></span></span></td>
-                        </tr>
-                        <tr>
-                            <td><span class="style4 style11 style13"><a href="Policies/Absent.pdf">Absenteeism Policy</a></span></td>
-                        </tr>
-                        <tr>
-                            <td><span class="style4 style11 style13"><a href="Policies/Anaphylaxis2016.pdf">Anaphylaxis Policy</a></span></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/Asthma.pdf" class="style4 style13">Asthma Policy</a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/BikeEd.pdf" class="style4 style13">Bike Education Policy </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/Bullying.pdf" class="style4 style13">Bullying - Children</a></td>
-                        </tr>
-                        <td><a href="Policies/ChildProtectionPolicy2016.pdf" class="style4 style13">Child Protection Policy </a></td>
-                        </tr>
-                        <td><a href="Policies/ChildSafetyStandardsJuly2016.pdf" class="style4 style13">Child Saftey Standards </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/Codeofconduct.pdf" class="style4 style13">Code of Conduct</a></td>
-                        </tr>
-                        <td><a href="Policies/Bullying.pdf" class="style4 style13">Bullying - Workplace Policy</a></td>
-                        </tr><tr>
 
-                            <td><a href="Policies/Disabilities.pdf" class="style4 style13">Disabilities &amp; Impairments </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/ElectronicRecording.pdf" class="style4 style13">Electronic Recording Policy</a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/EqualOpportunity.pdf" class="style4 style13">Equal Opportunity </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/EnrolmentAreaPolicy2016.pdf" class="style4 style13">Enrolment Area Policy 2016 </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/eSmart.pdf" class="style4 style13">eSmart/Cyberbullying/Cybersafety</a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/Excursions.pdf" class="style4 style13">Excursion Policy</a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/Firstaid.pdf" class="style4 style13">First Aid Policy</a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/Harassmentpolicy.pdf" class="style4 style13">Harrassment</a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/Headlice.pdf" class="style4 style13">Head Lice Policy</a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/Incursions.pdf" class="style4 style13">Incursion Policy</a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/Internetemail.pdf" class="style4 style13">Internet and email</a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/Investmentpolicy.pdf" class="style4 style13">Investment Policy </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/Mobile.pdf" class="style4 style13">Mobile Phones Policy </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/OHSpolicy.pdf" class="style4 style13">OH &amp; S  Policy </a></td>
-                        </tr>
-                        <tr>
-                            <td><span class="style4 style13"><a href="Policies/Parentcomplaintspolicy.pdf">Parent Complaints Policy</a></span></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/Parentalcomplaintsguidelines.pdf" class="style4 style13">Parental Complaints Guidelines</a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/parentpayments.pdf" class="style4 style13">Parental Payments and Contributions </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/sce.pdf" class="style4 style13">School Council Elections </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/Schoolphilosophy.pdf" class="style4 style13">School Philosophy</a></td>
-                        </tr>
-                        <tr>
+<div id="features-sec" class="container set-pad" >
+    <div class="row text-left">
+        <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+            <h1 class="header-line text-center">For Parents</h1>
+            <p>After following the appropriate processes the School Council is extremely pleased to announce the replacement of the existing five Play Systems with new ones from a leading playground company. If you would like to see the drawings for each year level please click the following links:</p>
+            <ul class ="text-center">
+                <li><span class="glyphicon glyphicon-ok"></span> PREPS  -<a href=""> 4611517 </a></li>
+                <li><span class="glyphicon glyphicon-ok"></span> YEAR 1 -<a href=""> 4611519 </a></li>
+                <li><span class="glyphicon glyphicon-ok"></span> YEAR 2 -<a href=""> 4611520 </a></li>
+                <li><span class="glyphicon glyphicon-ok"></span> YRS3/4 -<a href=""> 4611522 </a></li>
+                <li><span class="glyphicon glyphicon-ok"></span> YRS5/6 -<a href=""> 4611542 </a></li>
+            </ul>
+            <hr>
+            <h1 class="header-line text-center">CGPS Policies and Plans</h1>
+            <p>Many policies/guidelines are dictated from the Department of Education & Training. Their website is <a href="">http://www.education.vic.gov.au/</a></p>
+            <table border="1">
+                <tr>
+                    <td><span class="style4 style11 style7 style10"><span class="style11 style13"><a href="2014AIP.pdf">CGPS Annual Implementation Plan </a></span></span></td>
+                </tr>
+                <tr>
+                    <td><span class="style4 style11 style7 style10"><span class="style11 style13"><a href="Annualreport2015.pdf">CGPS Annual Report to the Community </a></span></span></td>
+                </tr>
+                <tr>
+                    <td><span class="style4 style11 style7 style10"><span class="style11 style13"><a href="2014StratPlan.pdf">CGPS Strategic Plan</a></span></span></td>
+                </tr>
+                <tr>
+                    <td><span class="style4 style11 style13"><a href="Policies/Absent.pdf">Absenteeism Policy</a></span></td>
+                </tr>
+                <tr>
+                    <td><span class="style4 style11 style13"><a href="Policies/Anaphylaxis2016.pdf">Anaphylaxis Policy</a></span></td>
+                </tr>
+                <tr>
+                    <td><a href="Policies/Asthma.pdf" class="style4 style13">Asthma Policy</a></td>
+                </tr>
+                <tr>
+                    <td><a href="Policies/BikeEd.pdf" class="style4 style13">Bike Education Policy </a></td>
+                </tr>
+                <tr>
+                    <td><a href="Policies/Bullying.pdf" class="style4 style13">Bullying - Children</a></td>
+                </tr>
+                <td><a href="Policies/ChildProtectionPolicy2016.pdf" class="style4 style13">Child Protection Policy </a></td>
+            </tr>
+            <td><a href="Policies/ChildSafetyStandardsJuly2016.pdf" class="style4 style13">Child Saftey Standards </a></td>
+        </tr>
+        <tr>
+            <td><a href="Policies/Codeofconduct.pdf" class="style4 style13">Code of Conduct</a></td>
+        </tr>
+        <td><a href="Policies/Bullying.pdf" class="style4 style13">Bullying - Workplace Policy</a></td>
+    </tr><tr>
 
-                            <td><a href="Policies/Studentdress.pdf" class="style4 style13">Student Dress &amp; Personal Appearance Policy</a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/Sunsmart.pdf" class="style4 style13">Sunsmart Policy</a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/VolunteersPolicy.pdf" class="style4 style13">Volunteers Policy</a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="Policies/Yard.pdf" class="style4 style13">Yard Supervision Policy</a></td>
-                        </tr>
-                        <tr>
+    <td><a href="Policies/Disabilities.pdf" class="style4 style13">Disabilities &amp; Impairments </a></td>
+</tr>
+<tr>
+    <td><a href="Policies/ElectronicRecording.pdf" class="style4 style13">Electronic Recording Policy</a></td>
+</tr>
+<tr>
+    <td><a href="Policies/EqualOpportunity.pdf" class="style4 style13">Equal Opportunity </a></td>
+</tr>
+<tr>
+    <td><a href="Policies/EnrolmentAreaPolicy2016.pdf" class="style4 style13">Enrolment Area Policy 2016 </a></td>
+</tr>
+<tr>
+    <td><a href="Policies/eSmart.pdf" class="style4 style13">eSmart/Cyberbullying/Cybersafety</a></td>
+</tr>
+<tr>
+    <td><a href="Policies/Excursions.pdf" class="style4 style13">Excursion Policy</a></td>
+</tr>
+<tr>
+    <td><a href="Policies/Firstaid.pdf" class="style4 style13">First Aid Policy</a></td>
+</tr>
+<tr>
+    <td><a href="Policies/Harassmentpolicy.pdf" class="style4 style13">Harrassment</a></td>
+</tr>
+<tr>
+    <td><a href="Policies/Headlice.pdf" class="style4 style13">Head Lice Policy</a></td>
+</tr>
+<tr>
+    <td><a href="Policies/Incursions.pdf" class="style4 style13">Incursion Policy</a></td>
+</tr>
+<tr>
+    <td><a href="Policies/Internetemail.pdf" class="style4 style13">Internet and email</a></td>
+</tr>
+<tr>
+    <td><a href="Policies/Investmentpolicy.pdf" class="style4 style13">Investment Policy </a></td>
+</tr>
+<tr>
+    <td><a href="Policies/Mobile.pdf" class="style4 style13">Mobile Phones Policy </a></td>
+</tr>
+<tr>
+    <td><a href="Policies/OHSpolicy.pdf" class="style4 style13">OH &amp; S  Policy </a></td>
+</tr>
+<tr>
+    <td><span class="style4 style13"><a href="Policies/Parentcomplaintspolicy.pdf">Parent Complaints Policy</a></span></td>
+</tr>
+<tr>
+    <td><a href="Policies/Parentalcomplaintsguidelines.pdf" class="style4 style13">Parental Complaints Guidelines</a></td>
+</tr>
+<tr>
+    <td><a href="Policies/parentpayments.pdf" class="style4 style13">Parental Payments and Contributions </a></td>
+</tr>
+<tr>
+    <td><a href="Policies/sce.pdf" class="style4 style13">School Council Elections </a></td>
+</tr>
+<tr>
+    <td><a href="Policies/Schoolphilosophy.pdf" class="style4 style13">School Philosophy</a></td>
+</tr>
+<tr>
 
-                    </table>
-                </div>
+    <td><a href="Policies/Studentdress.pdf" class="style4 style13">Student Dress &amp; Personal Appearance Policy</a></td>
+</tr>
+<tr>
+    <td><a href="Policies/Sunsmart.pdf" class="style4 style13">Sunsmart Policy</a></td>
+</tr>
+<tr>
+    <td><a href="Policies/VolunteersPolicy.pdf" class="style4 style13">Volunteers Policy</a></td>
+</tr>
+<tr>
+    <td><a href="Policies/Yard.pdf" class="style4 style13">Yard Supervision Policy</a></td>
+</tr>
+<tr>
 
-                <hr>
-            </div>
-        </div>
-    </div>
+</table>
+</div>
+
+<hr>
+</div>
+</div>
+
 @endsection
