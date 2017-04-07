124a62962baedfc1fabee747cb99ebfc0e70addf
Anthony <s3333113@student.rmit.edu.au>
2017-04-07 16:59:10 +1000

    removed laravelcollective dependency

--- resources/views/admin/events/create.blade.php
+++ resources/views/admin/events/create.blade.php
@@ -1,10 +1,27 @@
 @extends('layouts.admin')
 @section('title', 'Create new Event')
 @section('content')
+<form action="{{ route('admin.events.store') }}" method="post">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
     <div class="form-group">
-        {{ Form::model(null, ['method' => 'POST', 'route' => ['admin.events.store', null]]) }}
-            @include('admin.events.form')
-            {{ Form::submit('Create', ['class' => 'form-control btn btn-success']) }}
-        {{ Form::close() }}
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" class="form-control">
     </div>
+    <div class="form-group">
+        <label for="date">Date</label>
+        <input type="date" name="date" id="date" class="form-control"></div>
+    <div class="form-group">
+        <label for="start_time">Start time</label>
+        <input type="time" name="start_time" id="start_time" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="end_time">End time</label>
+        <input type="time" name="end_time" id="end_time" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="notes">Notes</label>
+        <textarea name="notes" id="notes" class="form-control"></textarea>
+    </div>
+    <input type="submit" value="Create new Event" class="btn btn-success">
+</form>
 @endsection
--- resources/views/admin/events/edit.blade.php
+++ resources/views/admin/events/edit.blade.php
@@ -1,15 +1,33 @@
 @extends('layouts.admin')
-@section('title', 'Edit ' . $event->id)
+@section('title', 'Edit ' . $event->name)
 @section('content')
-<div class="form-group">
-    {{-- Delete form --}}
-    {{ Form::model($event, ['method' => 'DELETE', 'route' => ['admin.events.destroy', $event]]) }}
-        {{ Form::submit('Delete ' . $event->name, ['class' => 'form-control btn btn-danger']) }}
-    {{ Form::close() }}
-
-    {{-- Update/edit form --}}
-    {{ Form::model($event, ['method' => 'PATCH', 'route' => ['admin.events.update', $event]]) }}
-        @include('admin.events.form')
-        {{ Form::submit('Save', ['class' => 'form-control btn btn-success']) }}
-    {{ Form::close() }}
+<form action="{{ route('admin.events.destroy', $event->id) }}" method="post">
+    <input type="hidden" name="_method" value="DELETE">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <input type="submit" value="Delete {{ $event->name }}" class="btn btn-danger">
+</form>
+<form action="{{ route('admin.events.update', $event->id) }}" method="post">
+    <input type="hidden" name="_method" value="PATCH">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" class="form-control" value="{{ $event->name }}">
+    </div>
+    <div class="form-group">
+        <label for="date">Date</label>
+        <input type="date" name="date" id="date" class="form-control"  value="{{ $event->date }}">
+    <div class="form-group">
+        <label for="start_time">Start time</label>
+        <input type="time" name="start_time" id="start_time" class="form-control" value="{{ $event->start_time }}">
+    </div>
+    <div class="form-group">
+        <label for="end_time">End time</label>
+        <input type="time" name="end_time" id="end_time" class="form-control"  value="{{ $event->end_time }}">
+    </div>
+    <div class="form-group">
+        <label for="notes">Notes</label>
+        <textarea name="notes" id="notes" class="form-control">{{ $event->notes }}</textarea>
+    </div>
+    <input type="submit" value="Update {{ $event->name }}" class="btn btn-success">
+</form>
 @endsection
