8ce0ec3279286ad2017a8aeb2e8290a3a2eb1905
Anthony <s3333113@student.rmit.edu.au>
2017-04-07 18:28:14 +1000

    updated with mass enable column

--- resources/views/admin/links/index.blade.php
+++ resources/views/admin/links/index.blade.php
@@ -3,13 +3,16 @@
 @section('content')
 <a href="{{ route('admin.links.create') }}" class="btn btn-info">Create new Link</a>
 <hr>
+<form action="{{ url('admin/links/massEnable') }}" method="post">
+<input type="hidden" name="_token" value="{{ csrf_token() }}">
 <table>
     <thead>
         <tr>
             <th>Name</th>
             <th>Active</th>
             <th>Parent</th>
-            <th>Action</th>
+            <th>Enable</th>
+            <th>Edit</th>
         </tr>
     </thead>
     <tbody>
@@ -26,9 +29,11 @@
                         {{ $link->parent->name }}
                     @endif
                 </td>
+                <td><input type="checkbox" name="{{ $link->id }}" value="1"></td>
                 <td><a href="{{ route('admin.links.edit', $link->id) }}" class="btn btn-primary">Edit</a></td>
             </tr>
         @endforeach
     </tbody>
 </table>
+<input type="submit" value="Enable ticked" class="btn btn-primary"></form>
 @endsection
