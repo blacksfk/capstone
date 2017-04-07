d970b64adbecfa3da3d87f70fa6a5e9c11dfb614
Anthony <s3333113@student.rmit.edu.au>
2017-04-07 14:31:43 +1000

    removed shared form

--- resources/views/admin/pages/form.blade.php
+++
@@ -1,9 +0,0 @@
-{{ Form::token() }}
-{{ Form::label('name', 'Name') }}
-{{ Form::text('name', null, ['class' => 'form-control']) }}
-{{ Form::label('category', 'Category') }}
-{{ Form::select('category', ['lol' => 'test', 'rofl' => 'test2'], null, ['class' => 'form-control', 'placeholder' => 'Select a link to bind to...']) }}
-{{ Form::label('status', 'Status') }}
-{{ Form::select('status', ['test' => 'test', 'test2' => 'test2'], null, ['class' => 'form-control', 'placeholder' => 'Select a status...']) }}
-{{ Form::label('content', 'Content') }}
-{{ Form::textarea('content', null, ['class' => 'form-control']) }}
