7524cd2fba1c92073a6ddd2eecd27da982d4d3f5
Anthony <s3333113@student.rmit.edu.au>
2017-04-11 13:13:09 +1000

    page now has a relationship with template (many to one)

--- app/Page.php
+++ app/Page.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Page extends Model
 {
-    protected $fillable = ["name", "link_id", "content"];
+    protected $fillable = ["name", "link_id", "template_id", "content"];
 
     public function link()
     {
         return $this->belongsTo("App\Link");
+    }
+
+    public function template()
+    {
+        return $this->belongsTo("App\Template");
     }
 }
