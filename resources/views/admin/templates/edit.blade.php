a1a846e9ee49c00a91864b66e9b5be08cf217c2f
Anthony <s3333113@student.rmit.edu.au>
2017-04-11 16:52:57 +1000

    fixed typo

--- resources/views/admin/templates/edit.blade.php
+++ resources/views/admin/templates/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin_form')
 @section('title', 'Editing ' . $template->name)
-@section('content')
+@section('form')
 <form action="{{ route('admin.templates.destroy', $template->id) }}" method="post">
     <input type="hidden" name="_method" value="DELETE">
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
@@ -9,7 +9,7 @@
 <form action="{{ route('admin.templates.update', $template->id) }}" method="post">
     <input type="hidden" name="_method" value="PATCH">
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
-    <div class="form-group"><label for="name">Template Name</label><input type="text" name="name" id="name" value="{{ $tempate->name }}" class="form-control"></div>
+    <div class="form-group"><label for="name">Template Name</label><input type="text" name="name" id="name" value="{{ $template->name }}" class="form-control"></div>
     <div class="form-group"><label for="content">Template Content</label><textarea name="content" id="content" cols="30" rows="10" class="form-control">{{ $template->content }}</textarea></div>
     <input type="submit" value="Update {{ $template->name }}" class="btn btn-success">
 </form>
