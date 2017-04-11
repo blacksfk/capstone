84ee01ea087c870cee63eb4f8b04d47a71a0169b
Anthony <s3333113@student.rmit.edu.au>
2017-04-11 13:55:39 +1000

    updated sidebar with link to template resource

--- resources/views/shared/sidebar.blade.php
+++ resources/views/shared/sidebar.blade.php
@@ -2,12 +2,12 @@
     <p><b>Administration</b></p>
     <ul class="nav navbar-nav">
         <li><a href="{{ url('admin') }}">Landing Page</a></li>
-        <li><a href="">Upload newsletter</a></li>
+        <li><a href="#">Upload newsletter</a></li>
         <li><a href="{{ route('admin.pages.index') }}">Page management</a></li>
         <li><a href="{{ route('admin.events.index') }}">Event management</a></li>
         <li><a href="{{ route('admin.links.index') }}">Link management</a></li>
-        <li><a href="">Edit calendar</a></li>
-        <li><a href="">Upload gallery images</a></li>
+        <li><a href="{{ route('admin.templates.index') }}">Template management</a></li>
+        <li><a href="#">Upload gallery images</a></li>
     </ul>
 </div>
 
