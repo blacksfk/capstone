e0d06dea1f5d01d603f1c27fb91ef509b846569f
Anthony <s3333113@student.rmit.edu.au>
2017-04-14 19:26:22 +1000

    created bootstrap modal

---
+++ resources/views/shared/modal.blade.php
@@ -0,0 +1,17 @@
+<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="popupModal" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body"></div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-danger">Confirm</button>
+            </div>
+        </div>
+    </div>
+</div>
