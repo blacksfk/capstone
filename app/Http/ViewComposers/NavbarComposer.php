ac7330734cc97c9c338886162f7b1b745394467a
Anthony <s3333113@student.rmit.edu.au>
2017-04-14 22:34:56 +1000

    created navbar service provider and composer

---
+++ app/Http/ViewComposers/NavbarComposer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\ViewComposers;
+
+use App\Link;
+use Illuminate\View\View;
+
+class NavbarComposer
+{
+    private $links = [];
+
+    /**
+     * Get all links in the database and sort them based on
+     * whether they are categories or standard links
+     */
+    public function __construct()
+    {
+        // get all links that don't have children
+        $categories = Link::where("parent_id", null)
+                            ->where("active", true)
+                            ->get();
+
+        // now get all their children
+        foreach ($categories as $category)
+        {
+            $this->links[$category->name] = 
+                Link::where("parent_id", $category->id)
+                        ->where("active", true)
+                        ->get();
+        }
+    }
+
+    /**
+     * Bind the links to the view
+     * @param  View   $view The view to be rendered
+     * @return void  
+     */
+    public function compose(View $view)
+    {
+        $view->with("dynLinks", $this->links);
+    }
+}
