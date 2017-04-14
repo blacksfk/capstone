30ff55d5a1c9016315dcc58f80fc85cb66cf0f4e
Anthony <s3333113@student.rmit.edu.au>
2017-04-14 12:32:34 +1000

    removed useless layout

--- resources/views/layouts/admin_form.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.admin')
-@section('content')
-<div class="pull-left">
-    <a href="{{ redirect()->back()->getTargetUrl() }}" class="btn btn-primary">Back</a>
-</div>
-@yield('form')
-@endsection
