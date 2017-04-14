6eb82f906188b43f30aae4eaeee28a6db1991ad0
Anthony <s3333113@student.rmit.edu.au>
2017-04-14 16:45:53 +1000

    updated table to reflect the unique attribute of template name

--- database/migrations/2017_04_11_030622_create_templates_table.php
+++ database/migrations/2017_04_11_030622_create_templates_table.php
@@ -14,7 +14,7 @@ class CreateTemplatesTable extends Migration
     {
         Schema::create('templates', function (Blueprint $table) {
             $table->increments('id');
-            $table->string("name");
+            $table->string("name")->unique();
             $table->string("content");
             $table->timestamps();
         });
