1a06f035eac930f293b14a453f1c973250f6c3c4
Anthony <s3333113@student.rmit.edu.au>
2017-04-14 18:16:59 +1000

    updated default template and seeder

--- database/seeds/TemplateSeeder.php
+++ database/seeds/TemplateSeeder.php
@@ -15,10 +15,16 @@ class TemplateSeeder extends Seeder
             "id" => 1,
             "name" => "default", 
             "content" => 
-                "@extends('layouts.master')" .
-                "@section('content')" .
-                "@yield('page_content')" .
-                "@endsection"
+                "@extends('layouts.master')
+                @section('content')
+                <div id='features-sec' class='container set-pad'>
+                    <div class='row text-center'>
+                        <div class='col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2'>
+                            <p>@yield('page_content')</p>
+                        </div>
+                    </div>
+                </div>
+                @endsection"
         ]);
     }
 }
