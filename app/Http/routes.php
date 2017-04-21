e81c1616a18c20b1eec4e76f51c2aaace7a6239c
Anthony <s3333113@student.rmit.edu.au>
2017-04-21 17:05:40 +1000

    updated admin route prefix to 5.3

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -42,7 +42,7 @@ Route::group(["middleware" => "auth"], function() {
     // dump auth only routes here
 });
 
-Route::group(["prefix" => "admin"], function() {
+Route::group(["as" => "admin.", "prefix" => "admin"], function() {
     Route::get("/", function() { return view("admin.dashboard"); });
     
     Route::resource("events", "EventController");
