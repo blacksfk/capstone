927d13eede5c41afc19420a08fd54b39ed002fd8
Anthony <s3333113@student.rmit.edu.au>
2017-04-26 00:30:13 +1000

    added docstring to page relationship function

--- app/Template.php
+++ app/Template.php
@@ -6,8 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Template extends Model
 {
-    protected $fillable = ["name", "content","sections"];
+    protected $fillable = ["name", "content", "sections"];
 
+    /**
+     * One to many relationship between this template and pages that
+     * reference it
+     * 
+     * @return Collection A collection of Page objects
+     */
     public function pages()
     {
         return $this->hasMany("App\Page");
