08c000535933fc69520e09fbd500cf777dfeba04
Reyham <s3496982@student.rmit.edu.au>
2017-05-03 23:12:47 +1000

    Added geolocation
    -added stalker mechanic.
    -initMap function now only constructs zone map when zone element is
    present on page.

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -39,7 +39,7 @@
         }
     </script>
     <script>
-      function initMap() {
+    function initMap() {
         var uluru = {lat: -38.079655, lng: 145.283334};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 15,
@@ -50,43 +50,74 @@
           map: map
         });
         
-        var zone = new google.maps.Map(document.getElementById('zone'), {
-          zoom: 15,
-          center: uluru
-        });
-        zoneACoordinates = [
-            {lat: -38.082223, lng: 145.276470},
-            {lat: -38.066036, lng: 145.266546},
-            {lat: -38.069698, lng: 145.298154},
-            {lat: -38.084608, lng: 145.295482}
-        ];
-        
-        zoneBCoordinates = [
-            {lat: -38.084608, lng: 145.295482},
-            {lat: -38.069698, lng: 145.298154},
-            {lat: -38.074546, lng: 145.335409},
-            {lat: -38.089474, lng: 145.332785}
-        ];
-        
-        var priorityZoneA = new google.maps.Polygon({
-          paths: zoneACoordinates,
-          strokeColor: '#FF000',
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: '#FF000',
-          fillOpacity: 0.35
-        });
-        
-        var priorityZoneB = new google.maps.Polygon({
-          paths: zoneBCoordinates,
-          strokeColor: '#FF000',
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: '#FF000',
-          fillOpacity: 0.35
-        });
-        priorityZoneA.setMap(zone);
-        priorityZoneB.setMap(zone);
+        if (document.getElementById('zone') !== null){
+            var zone = new google.maps.Map(document.getElementById('zone'), {
+              zoom: 15,
+              center: uluru
+            });
+            zoneACoordinates = [
+                {lat: -38.082223, lng: 145.276470},
+                {lat: -38.066036, lng: 145.266546},
+                {lat: -38.069698, lng: 145.298154},
+                {lat: -38.084608, lng: 145.295482}
+            ];
+            
+            zoneBCoordinates = [
+                {lat: -38.084608, lng: 145.295482},
+                {lat: -38.069698, lng: 145.298154},
+                {lat: -38.074546, lng: 145.335409},
+                {lat: -38.089474, lng: 145.332785}
+            ];
+            
+            var priorityZoneA = new google.maps.Polygon({
+              paths: zoneACoordinates,
+              strokeColor: '#ee1717',
+              strokeOpacity: 0.8,
+              strokeWeight: 2,
+              fillColor: '#ee1717',
+              fillOpacity: 0.35
+            });
+            
+            var priorityZoneB = new google.maps.Polygon({
+              paths: zoneBCoordinates,
+              strokeColor: '#2917ee',
+              strokeOpacity: 0.8,
+              strokeWeight: 2,
+              fillColor: '#2917ee',
+              fillOpacity: 0.35
+            });
+            priorityZoneA.setMap(zone);
+            priorityZoneB.setMap(zone);
+            
+            infoWindow = new google.maps.InfoWindow;
+            
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
+
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent('Location found.');
+                    infoWindow.open(zone);
+                    zone.setCenter(pos);
+                }, function() {
+                        handleLocationError(true, infoWindow, zone.getCenter());
+                    });
+            } else {
+                // Browser doesn't support Geolocation
+                handleLocationError(false, infoWindow, zone.getCenter());
+            }
+        }
+    }
+    
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(zone);
       }
     </script>
     <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
