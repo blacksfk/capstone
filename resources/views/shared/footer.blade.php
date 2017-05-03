7b627c068641d20173a5641a4ae34339baa95784
Reyham <s3496982@student.rmit.edu.au>
2017-05-03 22:42:40 +1000

    highway to the
    danger zone

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -38,7 +38,7 @@
           new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
         }
     </script>
-        <script>
+    <script>
       function initMap() {
         var uluru = {lat: -38.079655, lng: 145.283334};
         var map = new google.maps.Map(document.getElementById('map'), {
@@ -49,6 +49,44 @@
           position: uluru,
           map: map
         });
+        
+        var zone = new google.maps.Map(document.getElementById('zone'), {
+          zoom: 15,
+          center: uluru
+        });
+        zoneACoordinates = [
+            {lat: -38.082223, lng: 145.276470},
+            {lat: -38.066036, lng: 145.266546},
+            {lat: -38.069698, lng: 145.298154},
+            {lat: -38.084608, lng: 145.295482}
+        ];
+        
+        zoneBCoordinates = [
+            {lat: -38.084608, lng: 145.295482},
+            {lat: -38.069698, lng: 145.298154},
+            {lat: -38.074546, lng: 145.335409},
+            {lat: -38.089474, lng: 145.332785}
+        ];
+        
+        var priorityZoneA = new google.maps.Polygon({
+          paths: zoneACoordinates,
+          strokeColor: '#FF000',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: '#FF000',
+          fillOpacity: 0.35
+        });
+        
+        var priorityZoneB = new google.maps.Polygon({
+          paths: zoneBCoordinates,
+          strokeColor: '#FF000',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: '#FF000',
+          fillOpacity: 0.35
+        });
+        priorityZoneA.setMap(zone);
+        priorityZoneB.setMap(zone);
       }
     </script>
     <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
