efe28774c169e094f9734f685d9e89cec363aafd
Anthony <s3333113@student.rmit.edu.au>
2017-05-06 21:11:15 +1000

    fixed active link highlighting - now also highlights dropdowns

--- public/assets/js/changeActiveLink.js
+++ public/assets/js/changeActiveLink.js
@@ -1,1 +1,6 @@
-$("ul.nav a[href='" + window.location.href + "'").parent().addClass("active");
+var link = "ul.nav a[href='" + window.location.href + "'";
+$(link).parent().addClass("active");
+
+if ($(link).parents("ul").hasClass("dropdown-menu")) {
+    $(link).parents(".dropdown").addClass("active");
+}
