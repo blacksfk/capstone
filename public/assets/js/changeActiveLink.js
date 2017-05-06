92853a5d616505bc622c413b10cb811527fb4fac
Anthony <s3333113@student.rmit.edu.au>
2017-05-06 18:33:23 +1000

    active link is now displayed properly

--- public/assets/js/changeActiveLink.js
+++ public/assets/js/changeActiveLink.js
@@ -1,13 +1,1 @@
-
-    //alert(window.location.pathname.split('/'))
-    var string = $.trim(window.location.pathname);
-    $.each(string.split('/'), function( key, value ) {
-        if (value == ""){
-
-        }
-        else {
-            var val = value.toUpperCase();
-            $("a:contains(" + val + ")").css("background-color","rgba(28, 143, 175, 1)");
-            $("a:contains(" + val + ")").css("color" , "white");
-        }
-    });
+$("ul.nav a[href='" + window.location.href + "'").parent().addClass("active");
