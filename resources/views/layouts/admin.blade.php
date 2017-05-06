821ab51b78fe9063711e3ad402d4d315e4c88bb7
Anthony <s3333113@student.rmit.edu.au>
2017-05-06 23:35:44 +1000

    now uses external files for head and scripts

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,61 +2,29 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>@yield('title')</title>
-    <!-- BOOTSTRAP CORE STYLE CSS -->
-    <link href="">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    <!-- FONT AWESOME CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
-    <!-- FLEXSLIDER CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/flexslider.css') }}">
-    <!-- CUSTOM STYLE CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">   
-    <!-- Google Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css' />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    @include('shared.head')
 </head>
 <body>
     @include('shared.modal')
     @include('shared.navbar')
-    <div  class="tag-line" >
-        <div class="container">
-            <div class="row  text-center" >
-                <div class="col-lg-12  col-md-12 col-sm-12">      
-                    <h2 data-scroll-reveal="enter from the bottom after 0.1s" >
-                        <i class="fa fa-circle-o-notch"></i>
-                        @yield('title')
-                        <i class="fa fa-circle-o-notch"></i>
-                    </h2>
+    @include('shared.messages')
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">@yield('title')</h3>
+                    </div>
+                    <div class="panel-body">@yield('content')</div>
+                    @yield('table')
+                    <div class="panel-footer">@yield('form_nav')</div>
                 </div>
             </div>
         </div>
     </div>
-    @include('shared.messages')
-    <div id="features-sec" class="container set-pad">
-        @include('shared.sidebar')
-        <div class="well row text-center col-lg-offset-3">
-            @yield('content')
-        </div>
     </div>
     @include('shared.footer')
-
+    @include('shared.scripts')
     <script src="{{ asset('assets/js/admin.js') }}"></script>
-    <!--  Core Bootstrap Script -->
-    <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
-    <!--  Flexslider Scripts --> 
-    <script src="{{ asset('assets/js/jquery.flexslider.js') }}"></script>
-    <!--  Scrolling Reveal Script -->
-    <script src="{{ asset('assets/js/scrollReveal.js') }}"></script>
-    <!--  Scroll Scripts --> 
-    <script src="{{ asset('assets/js/jquery.easing.min.js') }}"></script>
-    <!--  Custom Scripts --> 
-    <script src="{{ asset('assets/js/custom.js') }}"></script>
-    <!--  Highlight current page -->
-    <script src="{{ asset('assets/js/changeActiveLink.js') }}"></script>
 </body>
 </html>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,33 +2,7 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width">
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>@yield('title')</title>
-    <!-- BOOTSTRAP CORE STYLE CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    <!-- FONT AWESOME CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
-    <!-- FLEXSLIDER CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/flexslider.css') }}">
-    <!-- CUSTOM STYLE CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">   
-    <!-- Google Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css' />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA1GEg7gRkcV2id4F8yoC0TtaW9Ok7jhx4&callback=initMap&libraries=places&cal"
-  type="text/javascript"></script>
-
-    <!-- CSRF token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <script>
-        window.Laravel = {!! json_encode([
-            'csrfToken' => csrf_token(),
-        ]) !!};
-    </script>
+    @include('shared.head')
 </head>
 <body>
     @include('shared.navbar')
@@ -51,24 +25,7 @@
     </div>
 
     @include('shared.footer')
-
-    <!--  Jquery Core Script -->
-    <!--  Core Bootstrap Script -->
-    <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
-    <!--  Flexslider Scripts --> 
-    <script src="{{ asset('assets/js/jquery.flexslider.js') }}"></script>
-    <!--  Scrolling Reveal Script -->
-    <script src="{{ asset('assets/js/scrollReveal.js') }}"></script>
-    <!--  Scroll Scripts --> 
-    <script src="{{ asset('assets/js/jquery.easing.min.js') }}"></script>
-    <!--  Custom Scripts --> 
-    <script src="{{ asset('assets/js/custom.js') }}"></script>
-    <!--  Highlight current link -->
-    <script src="{{ asset('assets/js/changeActiveLink.js') }}"></script>
-    <!-- Accordion -->
-    <script src="{{ asset('assets/js/accordion.js') }}"></script>
-
-    <script src="{{ asset('js/app.js') }}"></script>
+    @include('shared.scripts')
 </body>
 
 </html>
