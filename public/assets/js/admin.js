fda15d3f73ae1194a3d877d0d0afc504de7b1cfe
Anthony <s3333113@student.rmit.edu.au>
2017-05-07 20:07:02 +1000

    refactored, modularised page editing js, and added docstrings to functions

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -1,9 +1,20 @@
-// slide away alerts
-$(".alert").on("close.bs.alert", function(event) {
-    event.preventDefault();
-    $(this).slideUp();
-});
+/*================================================================
+    CONSTANTS
+ ===============================================================*/
+ const SLIDE_TIME = 700;
 
+
+ /*================================================================
+    FUNCTIONS
+ ===============================================================*/
+ /**
+  * Finds all of the checked input boxes, appends them to the form specified, 
+  * and then submits the form
+  * 
+  * @param  JSEvent event
+  * @param  String formID The ID attribute of the form that will be submitted
+  * @return void
+  */
 function toggleLinks(event, formID) {
     event.preventDefault();
     $("input:checked").each(function(index, element) {
@@ -12,6 +23,14 @@ function toggleLinks(event, formID) {
     $(formID).submit();
 }
 
+/**
+ * Creates and shows a deletion confirmation modal, and submits 
+ * the original form if the user clicks confirm
+ * 
+ * @param  JSEvent event  
+ * @param  String formID    The deletion form that will be submitted
+ * @return void
+ */
 function confirmDelete(event, formID) {
     event.preventDefault();
     
@@ -25,7 +44,76 @@ function confirmDelete(event, formID) {
     );
     $("#modalConfirm").addClass("btn-danger");
     $("#adminModal").modal("show");
-    $("#adminModal .btn-danger").click(function() {
+    $("#modalConfirm").click(function() {
         $(formID).submit();
     })
 }
+
+/**
+ * Builds an associative array, performs an ajax get request to the server,
+ * creates a window, and appends the result (a string to the newly created window)
+ * 
+ * @param  HTMLObject caller    An HTML object with an href property - required!!
+ * @param  JSEvent    event
+ * @return void
+ */
+function previewPage(caller, event) {
+    event.preventDefault();
+    var contentArray = {};
+
+    $("input[name^='content']").each(function(index, element) {
+        contentArray[element.id] = element.value;
+    });
+
+    $.get($(caller).prop("href"), {
+        id: $("#template_id").val(), 
+        name: $("#name").val(), 
+        content: JSON.stringify(contentArray)
+    }, 
+    function(data) {
+        var wdw = window.open();
+        wdw.document.write(data);
+    });
+}
+
+/**
+ * Appends the data received to the html object specified 
+ * (to be used with templates)
+ * 
+ * @param  JSON data    A JSON object with sections
+ * @return void            
+ */
+function appendSections(data) {
+    $.each(data, function(index, section) {
+        htmlString = "" +
+            "<div class='form-group'>" +
+            "<label for='content[" + section + "]'>" + section + "</label>" +
+            "<textarea name='content[" + section + "]' id=" + section + " cols='30' rows='10' class='form-control'></textarea>" +
+            "</div>"
+        $(htmlString).hide().appendTo("#inputs").slideDown(SLIDE_TIME);
+    }); 
+}
+
+
+/*================================================================
+    EVENT HANDLERS
+ ===============================================================*/
+ // slide away alerts (smoother)
+$(".alert").on("close.bs.alert", function(event) {
+    event.preventDefault();
+    $(this).slideUp();
+});
+
+// get all the sections for a template
+$("#template_id").change(function() {
+    if ($("#inputs").html() !== "") {
+        $("#inputs").html("");
+    }
+
+    // hidden field in the form to store the dynamic route to the template
+    $.get($("input[name='_template_route']").val(), 
+        {id: $("#template_id").val()}, 
+        appendSections, 
+        "json"
+    );
+});
