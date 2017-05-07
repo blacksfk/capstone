e9093d440a170c8171cf9b756eb9846fa2a67765
Anthony <s3333113@student.rmit.edu.au>
2017-05-07 14:26:33 +1000

    refactored delete confirmation and created toggle links function

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -1,45 +1,31 @@
-var deleteClicked = false;
-
-$("form").submit(function(event) {
-    if (deleteClicked === true) {
-        return;
-    }
-
-    var form = this;
-
-    // only show modal if the form action is delete
-    if ($("input[name='_method']", form).length === 0 || $("input[name='_method']", form).val().toUpperCase() !== "DELETE") {
-        return;
-    }
-
-    event.preventDefault();
-    deleteClicked = true;
-    var name = $(form).find("input[name='record']");
-
-    $(".modal-header").text("Confirm deletion");
-
-    // extract the name of the item from the button
-    $(".modal-body").text(
-        "Are you sure you want to delete " + 
-        $(name).val() +  
-        "? This action cannot be undone!"
-    );
-    $("#adminModal").modal("show")
-
-    // trigger the form action on click confirmation
-    $(".modal-footer > .btn-danger").click(function() {
-        $("#adminModal").modal("hide");
-        $(form).submit();
-    });
-});
-
-// reset flag when modal is hidden
-$("#adminModal").on("hidden.bs.modal", function() {
-    deleteClicked = false;
-});
-
 // slide away alerts
 $(".alert").on("close.bs.alert", function(event) {
     event.preventDefault();
     $(this).slideUp();
 });
+
+function toggleLinks(event, formID) {
+    event.preventDefault();
+    $("input:checked").each(function(index, element) {
+        $(formID).append(element);
+    });
+    $(formID).submit();
+}
+
+function confirmDelete(event, formID) {
+    event.preventDefault();
+    
+    var name = $(formID).find("input[name='record']");
+
+    $(".modal-header").text("Confirm deleteion");
+    $(".modal-body").text(
+        "Are you sure you want to delete " +
+        $(name).val() +
+        "? This action cannot be undone!"
+    );
+    $("#adminModal").modal("show");
+    $(".modal-footer > .btn-danger").click(function() {
+        $("#adminModal").modal("hide");
+        $(formID).submit();
+    })
+}
