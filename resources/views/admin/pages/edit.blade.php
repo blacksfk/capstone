b07760b640d94a4780bf1a8f55e2c632af0462a9
Anthony <s3333113@student.rmit.edu.au>
2017-05-07 20:08:10 +1000

    added hidden field for js to grab url for templates.sections

--- resources/views/admin/pages/edit.blade.php
+++ resources/views/admin/pages/edit.blade.php
@@ -27,6 +27,8 @@
         </select>
     </div>
     <div class="form-group">
+        {{-- This hidden field is required if the JS is going to be external --}}
+        <input type="hidden" name="_template_route" value="{{ route('admin.templates.sections') }}">
         <label for="template_id">Template</label>
         <select name="template_id" id="template_id" class="form-control">
             @foreach ($templates as $template)
