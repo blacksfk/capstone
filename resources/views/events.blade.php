63c0ea4fcfe8546a2f808ad414c55b32256cd734
Anthony <s3333113@student.rmit.edu.au>
2017-05-10 18:10:40 +1000

    table is now responsive on mobile

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -5,17 +5,18 @@
 <div id="features-sec" class="container set-pad" >
     <div class="row text-center">
         <h1 class="header-line">Events</h1>
-        <table class="table table-hover template_table">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Name</th>
-                    <th>Start time</th>
-                    <th>End time</th>
-                    <th>Notes</th>
-                </tr>
-            </thead>
-            <tbody>
+        <div class="table-responsive">
+            <table class="table table-hover template_table">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Name</th>
+                        <th>Start time</th>
+                        <th>End time</th>
+                        <th>Notes</th>
+                    </tr>
+                </thead>
+                <tbody>
                 @foreach ($events as $event)
                     <tr>
                         <td>{{ $event->date }}</td>
@@ -25,8 +26,9 @@
                         <td>{{ $event->notes }}</td>
                     </tr>
                 @endforeach
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 @endsection
