c997de6e34611bec59348ee6de431e882026c71d
Ulyanov Chua <s3501278@student.rmit.edu.au>
2017-05-12 15:56:24 +1000

    commit before merge, changed changeActiveLink.js

--- public/assets/js/changeActiveLink.js
+++ public/assets/js/changeActiveLink.js
@@ -9,5 +9,8 @@
             var val = value.toUpperCase();
             $("a:contains(" + val + ")").css("background-color","rgba(28, 143, 175, 1)");
             $("a:contains(" + val + ")").css("color" , "white");
+            //$("ul:contains(" + val + ")").removeClass();
+            //$("ul:contains(" + val + ")").addClass( "static-sliding" );
+
         }
     });
