2e36480d2e01a3234e47035aba52226d1271ccee
Anthony <s3333113@student.rmit.edu.au>
2017-05-12 21:06:43 +1000

    modified public driver to point to public/assets directory instead

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,10 +48,16 @@ return [
             'root' => storage_path('app'),
         ],
 
-        'public' => [
+        /*'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'visibility' => 'public',
+        ],*/
+
+        "public" => [
+            "driver" => "local",
+            "root" => public_path("assets"),
+            "visibility" => "public",
         ],
 
         's3' => [
