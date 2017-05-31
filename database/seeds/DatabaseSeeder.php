be6586eafda26c14d456e9801cadf255cff2314a
blacksfk <s3333113@student.rmit.edu.au>
2017-05-31 23:02:15 +1000

    created carousel seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(TemplateSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(AssetSeeder::class);
+        $this->call(CarouselSeeder::class);
     }
 }
