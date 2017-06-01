3d1fe98bb1207b0a298528886382f2158498a442
blacksfk <s3333113@student.rmit.edu.au>
2017-06-01 18:45:34 +1000

    changed types declaration from static to constant

--- app/Asset.php
+++ app/Asset.php
@@ -6,8 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Asset extends Model
 {
+    // these constants store the directory name in public/assets/
+    const TYPE_IMAGE = "img";
+    const TYPE_VIDEO = "video";
+    const TYPE_PDF = "pdf";
+    
+    // stores all of the types for selection, Asset::TYPES
+    const TYPES = [
+        TYPE_IMAGE => "Image", 
+        TYPE_VIDEO => "Video", 
+        TYPE_PDF => "PDF"
+    ];
+
+    
     protected $fillable = ["name", "type"];
-    public static $assetTypes = ["img" => "Image", "video" => "Video", "pdf" => "PDF"];
 
     public function carouselItem()
     {
