f726f7021044b48afdeeccbd85a90f1791224d20
blacksfk <s3333113@student.rmit.edu.au>
2017-06-02 18:06:00 +1000

    added filter option

--- resources/views/admin/assets/index.blade.php
+++ resources/views/admin/assets/index.blade.php
@@ -2,9 +2,19 @@
 @section('title', 'Asset management')
 @section('content')
 <a href="{{ route('admin.assets.create') }}" class="btn btn-primary">Upload new Asset</a>
+<hr>
+<div class="form-group">
+    <label for="asset_filter">Filter</label>
+    <select name="asset_filter" id="asset_filter" class="form-control">
+        <option value="">None</option>
+    @foreach ($types as $k => $v)
+        <option value="{{ $k }}">{{ $v }}</option>
+    @endforeach
+    </select>
+</div>
 @endsection
 @section('table')
-<div class="table-responsive">
+<div class="table-responsive" id="assets_table">
     <table class="table table-hover">
         <thead>
             <th>Name</th>
@@ -18,12 +28,15 @@
                     <td>{{ $asset->name }}</td>
                     <td>{{ $asset->type }}</td>
                     <td>
-                        @if ($asset->type === "img")
+                        @if ($asset->type === App\Asset::TYPE_IMAGE)
                             <img src="{{ asset('assets/' . $asset->type . '/' . $asset->name) }}" alt="{{ $asset->name }}" class="img-thumbnail" height="200" width="200">
-                        @elseif ($asset->type === "video")
+                        @elseif ($asset->type === App\Asset::TYPE_VIDEO)
                             <video controls class="embed-responsive-item img-thumbnail">
                                 <source src="{{ asset('assets/' . $asset->type . '/' . $asset->name) }}">
                             </video>
+                            <object width='100%' height='400' data='" + link + "'></object>";
+                        @else
+                            <object data="{{ asset('assets/' . $asset->type . '/' . $asset->name) }}" width="200px" height="200px"><a href="{{ asset('assets/' . $asset->type . '/' . $asset->name) }}">{{ $asset->name }}</a></object>
                         @endif
                     </td>
                     <td>
