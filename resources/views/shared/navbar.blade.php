12099a8fb305e7972ef8e0f06f1e7f4e6deedaaa
blacksfk <s3333113@student.rmit.edu.au>
2017-06-03 02:20:41 +1000

    removed link to template management

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -80,7 +80,6 @@
                             <li><a href="{{ route('admin.events.index') }}">Event management</a></li>
                             <li><a href="{{ route('admin.links.index') }}">Link management</a></li>
                             <li><a href="{{ route('admin.pages.index') }}">Page management</a></li>
-                            <li><a href="{{ route('admin.templates.index') }}">Template management</a></li>
                         </ul>
                     </li>
                     <li><a href="{{ url('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">LOGOUT</a></li>
