b530cdb4ae6c490fc594f3f29a8d2050444bba46
blacksfk <s3333113@student.rmit.edu.au>
2017-06-07 18:37:54 +1000

    removed unique

--- app/Http/Requests/CarouselPost.php
+++ app/Http/Requests/CarouselPost.php
@@ -24,7 +24,7 @@ class CarouselPost extends FormRequest
     public function rules()
     {
         return [
-            "asset_id" => "required|unique",
+            "asset_id" => "required",
             "caption" => "required",
         ];
     }
