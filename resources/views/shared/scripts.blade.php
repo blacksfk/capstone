4ae349ac02b1df6a0c41a214add4de04681a7031
Reyham <s3496982@student.rmit.edu.au>
2017-06-09 15:16:43 +1000

    scripts.blade.php change

--- resources/views/shared/scripts.blade.php
+++ resources/views/shared/scripts.blade.php
@@ -1,16 +1,6 @@
 <!--  Core Bootstrap Script -->
 <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
-<!--  Flexslider Scripts --> 
-<script src="{{ asset('assets/js/jquery.flexslider.js') }}"></script>
-<!--  Scrolling Reveal Script -->
-<script src="{{ asset('assets/js/scrollReveal.js') }}"></script>
-<!--  Scroll Scripts --> 
-<script src="{{ asset('assets/js/jquery.easing.min.js') }}"></script>
 <!--  Custom Scripts --> 
 <script src="{{ asset('assets/js/custom.js') }}"></script>
-<!--  Highlight current link -->
-<script src="{{ asset('assets/js/changeActiveLink.js') }}"></script>
-<!-- Accordion -->
-<script src="{{ asset('assets/js/accordion.js') }}"></script>
-<!-- random carousel image -->
-<script src="{{ asset('assets/js/carousel-dynamic.js') }}"></script>
+<!-- functions.js -->
+<script src="{{ asset('assets/js/functions.js') }}"></script>
