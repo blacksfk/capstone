0904bc8574b43ad6d79e48b122efcbc35286a3ea
Ulyanov Chua <s3501278@student.rmit.edu.au>
2017-06-09 12:49:55 +1000

    fixed the layout of newsletters

--- resources/views/parents-info/newsletters.blade.php
+++ resources/views/parents-info/newsletters.blade.php
@@ -10,15 +10,19 @@
             Each issue can be downloaded by clicking on the links below.</p>
     </div>
     <div class="row text-center">
+        <div class="col-sm-1"></div>
         <div class="well col-sm-2">
             @foreach ($newsletters as $newsletter)
                 <a class="pdfLink" href="{{ asset('assets/' . $newsletter->type . '/' . $newsletter->name) }}">{{ $newsletter->name }}</a><br>
             @endforeach
         </div>
-        
-        <div class="well row col-lg-offset-3" id="pdfContainer">
-            PLACEHOLDER
+
+        <div class="col-sm-1"></div>
+
+        <div class="well row col-sm-7" id="pdfContainer">
+            select an issue on the left to begin!
         </div>
+        <div class="col-sm-1"></div>
     </div>
 </div>
 @endsection
