314727a40216630cee5c3e66914d5b42b1306959
blacksfk <s3333113@student.rmit.edu.au>
2017-06-09 15:23:33 +1000

    moved functions from controller to home controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Event;
-use App\Page;
-use App\Asset;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -14,57 +11,4 @@ use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    /**
-     * Show all of the events in the database to the user ordered by date
-     * 
-     * @return View
-     */
-    public function events()
-    {
-        $events = Event::orderBy("date", "asc")->get();
-
-        return view("events")->with("events", $events);
-    }
-
-    /**
-     * Tries to find the page name matching the route
-     * 
-     * @param  string $page_name
-     * @return View
-     */
-    public function dynamic($page_name)
-    {
-        $page = Page::where("name", $page_name)->first();
-
-        if (!isset($page))
-        {
-            abort(404);
-        }
-
-        return view($page->name);
-    }
-
-    /**
-     * Shows all of the newsletters in the database to the user ordered by name
-     * 
-     * @return View
-     */
-    public function newsletters()
-    {
-        $nl = Asset::where("type", Asset::TYPE_NEWSLETTER)->orderBy("name")->get();
-
-        return view("parents-info.newsletters")->with("newsletters", $nl);
-    }
-
-    /**
-     * Retrieves the canteen menu from the database
-     * 
-     * @return View
-     */
-    public function canteenMenu()
-    {
-        //
-    }
-
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Page;
+use App\Asset;
+use App\Event;
 use App\Utility;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+
 
 class HomeController extends Controller
 {
@@ -25,6 +30,58 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    /**
+     * Show all of the events in the database to the user ordered by date
+     * 
+     * @return View
+     */
+    public function events()
+    {
+        $events = Event::orderBy("date", "asc")->get();
+
+        return view("events")->with("events", $events);
+    }
+
+    /**
+     * Tries to find the page name matching the route
+     * 
+     * @param  string $page_name
+     * @return View
+     */
+    public function dynamic($page_name)
+    {
+        $page = Page::where("name", $page_name)->first();
+
+        if (isset($page) && View::exists("custom." . $page->name))
+        {
+            return view("custom." . $page->name);
+        }
+
+        abort(404);
+    }
+
+    /**
+     * Shows all of the newsletters in the database to the user ordered by name
+     * 
+     * @return View
+     */
+    public function newsletters()
+    {
+        $nl = Asset::where("type", Asset::TYPE_NEWSLETTER)->orderBy("name")->get();
+
+        return view("parents-info.newsletters")->with("newsletters", $nl);
+    }
+
+    /**
+     * Retrieves the canteen menu from the database
+     * 
+     * @return View
+     */
+    public function canteenMenu()
+    {
+        //
     }
 
 }
