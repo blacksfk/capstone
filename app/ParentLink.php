42305f09e85a56a1121a9c4f06a324c0523221d4
blacksfk <s3333113@student.rmit.edu.au>
2017-06-10 21:38:14 +1000

    now checks whether to get all children based on boolean

--- app/ParentLink.php
+++ app/ParentLink.php
@@ -11,10 +11,18 @@ class ParentLink
     private $linkObject;
     private $children;
 
-    public function __construct($link)
+    public function __construct($link, $is_navbar = true)
     {
         $this->linkObject = $link;
-        $this->children = $link->children->where("active", true);
+
+        if ($is_navbar)
+        {
+            $this->children = $link->children->where("active", true);
+        }
+        else
+        {
+            $this->children = $link->children;
+        }
     }
 
     public function getLink()
