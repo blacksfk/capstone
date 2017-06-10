0918c3e111cbf75e3f0d0f4bb0a95757e384b382
blacksfk <s3333113@student.rmit.edu.au>
2017-06-10 21:19:27 +1000

    nows uses link name

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -20,12 +20,12 @@
                         <a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="dropdown-toggle">{{ strtoupper($link->getLink()->name) }}<span class="caret"></span></a>
                         <ul class="dropdown-menu">
                     @foreach ($link->getChildren() as $child)
-                            <li><a href="{{ url(strtolower($child->page->name)) }}">{{ $child->page->name }}</a></li>
+                            <li><a href="{{ url(strtolower($child->name)) }}">{{ $child->name }}</a></li>
                     @endforeach
                         </ul>
                     </li>
                 @else
-                    <li><a href="{{ url(strtolower($link->getLink()->page->name)) }}">{{ strtoupper($link->getLink()->page->name) }}</a></li>
+                    <li><a href="{{ url(strtolower($link->getLink()->name)) }}">{{ strtoupper($link->getLink()->name) }}</a></li>
                 @endif
             @endforeach
                 <!-- Authentication Links -->
