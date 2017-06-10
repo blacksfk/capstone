f0cd070e616802de1200ee914b69fc6ea0b1a8a8
blacksfk <s3333113@student.rmit.edu.au>
2017-06-10 23:24:29 +1000

    removed call to custom.js

--- resources/views/shared/scripts.blade.php
+++ resources/views/shared/scripts.blade.php
@@ -1,6 +1,4 @@
 <!--  Core Bootstrap Script -->
 <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
-<!--  Custom Scripts --> 
-<script src="{{ asset('assets/js/custom.js') }}"></script>
 <!-- functions.js -->
 <script src="{{ asset('assets/js/functions.js') }}"></script>
