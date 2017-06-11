e0d24c9e159358ce3385089b938af716fb298067
blacksfk <s3333113@student.rmit.edu.au>
2017-06-11 18:24:45 +1000

    no script now redirects on refresh when js is enabled

--- resources/views/noscript.blade.php
+++ resources/views/noscript.blade.php
@@ -12,6 +12,14 @@
             color: #B90000;
             size: 200%;
         }
+
+        h2 {
+            color: #E50000;
+            size: 150%;
+        }
     </style>
     <div><h1>Error: Please enable Javascript</h1></div>
+    <script>
+        window.location.replace(window.location.origin);
+    </script>
 </body>
