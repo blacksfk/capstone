3a058fbd4a1f91ee59ab824d63ca538ff000276d
blacksfk <s3333113@student.rmit.edu.au>
2017-06-12 16:41:49 +1000

    removed upload link

--- resources/views/admin/backups/index.blade.php
+++ resources/views/admin/backups/index.blade.php
@@ -1,15 +1,10 @@
 @extends('layouts.admin')
 @section('title', 'Backup and Restore')
 @section('content')
-<div class="row">
-    <div class="col-xs-12">
-        <form action="{{ route('admin.backups.backup') }}" method="post">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            <input type="submit" value="Create Backup" class="btn btn-default">
-        </form>
-        <a href="{{ route('admin.backups.create') }}" class="btn btn-default">Upload Backup</a>
-    </div>
-</div>
+<form action="{{ route('admin.backups.backup') }}" method="post">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <input type="submit" value="Create Backup" class="btn btn-default">
+</form>
 @endsection
 @section('table')
 <div class="table-responsive">
