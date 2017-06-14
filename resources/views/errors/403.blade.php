19095facf3ab3ae4b5ef3d4230dc155ca4f26079
blacksfk <s3333113@student.rmit.edu.au>
2017-06-14 22:17:43 +1000

    added 403 error page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('title', 'Page not found')
+@section('content')
+<div class="container">
+    <div class="row text-center">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h2>Sorry, you don't have permission to update that resource</h2>
+        </div>
+    </div>
+</div>
+@endsection
