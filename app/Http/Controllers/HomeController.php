9d0987d21606625ca105fb021af2d4edf7df3e04
Ulyanov Chua <s3501278@student.rmit.edu.au>
2017-06-15 07:28:08 +1000

    finally fixed the mail and smtp compatibility issues

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -70,11 +70,21 @@ class HomeController extends Controller
 
     public function sendEmail(Request $request)
     {
+        $title = $request->name . ' ' . $request->surname ;
+        $email = $request->email;
+        $phone = $request->phone;
+        $content = $request->message;
+        Mail::send('emails.send', ['title' => $title, 'email' => $email, 'phone' => $phone,'content' => $content], function ($message)
+        {
+            $message->subject('Enquiry');
+
+            $message->from('comment@gmail.com', 'Enquiry Box');
+
+            $message->to('cgpsnew1@gmail.com');
+
+        });
 
 
-
-
-        return redirect()->route("index")
-            ->with(Messages::SUCCESS, Messages::CONTACT[Messages::SUCCESS]);
+        return redirect()->route("index")->with(Messages::SUCCESS, Messages::CONTACT[Messages::SUCCESS]);
     }
 }
---
+++ resources/views/emails/send.blade.php
@@ -0,0 +1,9 @@
+<html>
+<head></head>
+<body>
+<h1>Enquiry from {{$title}}</h1>
+<div>Email address: {{$email}}</div>
+<div>Phone number: {{$phone}}</div>
+<div>The message: {{$content}}</div>
+</body>
+</html>
